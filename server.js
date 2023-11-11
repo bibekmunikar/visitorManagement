eed35f789adf05d2791c2b5337f7748a41ea9b7c
Bibek Munikar <bibek.munikar@gmail.com>
2023-11-11 18:39:44 +1300

    integrated bootstrap, created view files
    integrated bootstrap,
    created view files for
    login
    registration
    admin dashboard

--- server.js
+++ server.js
@@ -4,17 +4,37 @@ const app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+//using app.use to erve up static css files in public /assets/ folder when /public link is called in ejs files 
+//app.use("/route", express.static("foldername"));
+app.use('/src', express.static('src'));  
+
 // use res.render to load up an ejs view file
 
-// index page
+// login page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/login');
 });
+
+//register page
+app.get('/register', function(req, res) {
+  res.render('pages/register');
+});
+
+//Dashboard page
+app.get('/dashboard', function(req, res) {
+  res.render('pages/dashboard');
+});
+
+
+// login page
+// app.get('/login', function(req, res) {
+//   res.render('pages/login');
+// });
 
 // about page
 app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
-app.listen(4000);
-console.log('Server is listening on port 4000');
+app.listen(3000);
+console.log('Server is listening on port 3000');
