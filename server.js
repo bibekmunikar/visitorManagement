e12861c2048d0bd761c8c3dde8984bb8d2c6d71f
Bibek Munikar <bibek.munikar@gmail.com>
2023-12-16 21:06:50 +1300

    changes in css, employees, departments,

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
+
+//File Upload
+const multer = require('multer');
+//Alert message
+const flash = require('express-flash');
+
 const path = require('path');
 const conn = require('./dbConfig');
 // const { Collapse } = require('bootstrap');
 // const { Console } = require('console');
 const app = express();
+
+// Initialize flash
+app.use(flash());
+
+// Set up Multer storage
+const storage = multer.diskStorage({
+    destination: 'uploads/', // Specify the upload directory
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname); // Set the filename to be unique
+    }
+});
+
+const upload = multer({ storage: storage })
 
 // set the view engine to ejs
 // view engine setup
@@ -90,7 +109,7 @@ app.get('/logout',(req,res) => {
 });
 
 // Users can access this if they are logged in
-app.get('/dashboard', function (req, res, next) {
+app.get('/dashboard', function (req, res) {
 		if (req.session.loggedin) {
     res.render('pages/dashboard');
 		} else {
@@ -99,7 +118,7 @@ app.get('/dashboard', function (req, res, next) {
 });
 
 //Visitors 
-app.get('/visitors', function(req, res, next) {
+app.get('/visitors', function(req, res) {
 	if (req.session.loggedin) {
 	res.render('pages/visitors');
 } else {
@@ -113,39 +132,100 @@ app.get('/visitorcontrol', function(req, res) {
 });
 
 //Departments page
-app.get('/departments', function(req, res, next) {
+// app.get('/departments', function(req, res) {
+// 	if (req.session.loggedin) {
+
+// 		const sql = 'SELECT * FROM departments';
+// 		conn.query(sql, (err, data) => {
+// 			console.log(data);
+// 			if (err) throw err;
+// 			res.render('departments', { departmentData: data });
+
+// 		});
+
+//   res.render('pages/departments');
+// } else {
+// 	res.redirect('/');
+// }
+// });
+
+app.get('/departments', function(req, res) {
 	if (req.session.loggedin) {
-  res.render('pages/departments');
-} else {
-	res.redirect('/');
-}
-});
+	  const sql = 'SELECT * FROM departments';
+	  conn.query(sql, (err, data) => {
+		if (err) throw err;
+		console.log(data);
+		res.render('pages/departments', { departmentData: data });
+	  });
+	} else {
+	  res.redirect('/');
+	}
+  });
 
 //Adding Departments
-app.post('/departments', function(req, res, next) {
+app.post('/add_departments', function(req, res) {
 	var department_name = req.body.department_name;
 	if (department_name) {
-		var sql = `INSERT INTO departments (department_name) VALUES ("${department_name}")`;
-		conn.query(sql, function(err, result) {
-			if (err) throw err;
-			console.log('record inserted');
-			res.render('pages/departments');
-		})
+	   var sql = 'INSERT INTO departments (department_name) VALUES (?)';
+	   conn.query(sql, [department_name], function(err, result) {
+		  if (err) {
+			 console.error(err);
+			 res.status(500).send('Internal Server Error');
+		  } else {
+			 console.log('Record inserted');
+			 res.redirect('/departments');
+		  }
+	   });
+	} else {
+	   console.log("Error: Department name is missing");
+	   res.status(400).send('Bad Request');
 	}
-	else {
-		// res.send('HELLO');
-		console.log("Error");
-	}
-});
+ });
+
 
 //Employees page
-app.get('/employees', function(req, res, next) {
+app.get('/employees', function(req, res) {
 	if (req.session.loggedin) {
-  res.render('pages/employees');
-} else {
-	res.redirect('/');
-}
+		const sql = 'SELECT * FROM employees';
+		conn.query(sql, (err, data) => {
+		  if (err) throw err;
+		  console.log(data);
+		  res.render('pages/employees', { employeesData: data });
+		});
+	  } else {
+		res.redirect('/');
+	  }
 });
+
+//Adding Employees
+app.post('/add_employees', upload.single('photo'), function(req, res) {
+	var full_name = req.body.full_name;
+	var alt_name = req.body.alt_name;
+	var email = req.body.email;
+	var mobile_number = req.body.mobile_number;
+	var department = req.body.department;
+	var job_title = req.body.job_title;
+	var emp_role_type = req.body.emp_role_type;
+	 // Access the file data using req.file
+	 var photo = req.file;
+
+	if (full_name && department) {
+		var sql = `INSERT INTO employees (full_name, alt_name, email, mobile_number, department, job_title, emp_role_type, photo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+	   conn.query(sql, [full_name, alt_name, email, mobile_number, department, job_title, emp_role_type, photo], function(err, result) {
+		  if (err) {
+			 console.error(err);
+			 res.status(500).send('Internal Server Error');
+		  } else {
+			 console.log('Record inserted');
+			 res.redirect('/employees');
+		  }
+	   });
+	} else {
+	   console.log("Error: Employee name is missing");
+	   res.status(400).send('Bad Request');
+	}
+ });
+
 
 //Dashboard page
 app.get('/dashboard', function(req, res) {
