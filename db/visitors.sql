66c3417629e74d6288c14e7ffdf6d052a951db66
Bibek Munikar <bibek.munikar@gmail.com>
2024-01-23 13:42:13 +1300

    signin page button added

--- db/visitors.sql
+++ db/visitors.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 14, 2024 at 02:51 PM
+-- Generation Time: Jan 23, 2024 at 01:36 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -33,8 +33,8 @@ CREATE TABLE `visitors` (
   `from` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` int(15) NOT NULL,
-  `visiting_employee_id` int(11) DEFAULT NULL,
-  `health_safety` varchar(255) DEFAULT NULL,
+  `employee_id` int(11) DEFAULT NULL,
+  `health_safety` varchar(128) NOT NULL,
   `checkin_time` timestamp NOT NULL DEFAULT current_timestamp(),
   `checkout_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -43,18 +43,13 @@ CREATE TABLE `visitors` (
 -- Dumping data for table `visitors`
 --
 
-INSERT INTO `visitors` (`id`, `name`, `from`, `email`, `phone`, `visiting_employee_id`, `health_safety`, `checkin_time`, `checkout_time`) VALUES
-(5, 'ashma', 'abc', 'abz@gmail.com', 0, NULL, NULL, '2024-01-13 06:55:53', '0000-00-00 00:00:00'),
-(9, 'bibek munikar', 'atrax group', 'bibek.munikar@gmail.com', 2041677367, 15, NULL, '2024-01-13 08:00:23', '0000-00-00 00:00:00'),
-(10, 'Soumya', 'atrax group', 'soumya@gmail.com', 93948834, 16, NULL, '2024-01-13 08:00:56', '0000-00-00 00:00:00'),
-(12, 'John Wick ', 'ajsjsjsjs', 'bibek.munikar@gmail.com', 2041677367, 22, NULL, '2024-01-13 08:03:01', '0000-00-00 00:00:00'),
-(13, 'bibek munikarasdas', 'asdasd', 'bibek.munikssasdaar@gmail.com', 234234234, 20, NULL, '2024-01-13 09:34:00', '0000-00-00 00:00:00'),
-(14, 'bibek munikar', '', 'bibek.munikar@gmail.com', 2041677367, NULL, NULL, '2024-01-14 07:03:05', '0000-00-00 00:00:00'),
-(15, 'Jackson Smith', 'Skycity', 'jacky@gmail.com', 993848393, NULL, NULL, '2024-01-14 12:55:53', '0000-00-00 00:00:00'),
-(16, 'bibek munikar', '', 'bibek.munikar@gmail.com', 2041677367, NULL, NULL, '2024-01-14 13:29:58', '0000-00-00 00:00:00'),
-(17, 'bibek munikar', '', 'bibek.munikar@gmail.com', 2041677367, NULL, NULL, '2024-01-14 13:36:15', '0000-00-00 00:00:00'),
-(18, 'bibek munikar', '', 'bibek.munikar@gmail.com', 2041677367, NULL, NULL, '2024-01-14 13:46:56', '0000-00-00 00:00:00'),
-(19, 'bibek munikar', 'atrax group', 'bibek.munikar@gmail.com', 2041677367, NULL, NULL, '2024-01-14 13:48:54', '0000-00-00 00:00:00');
+INSERT INTO `visitors` (`id`, `name`, `from`, `email`, `phone`, `employee_id`, `health_safety`, `checkin_time`, `checkout_time`) VALUES
+(32, 'bibek munikar', 'atrax group', 'bibek.munikar@gmail.com', 2041677367, 28, '1', '2024-01-20 12:28:58', '0000-00-00 00:00:00'),
+(35, 'John Wick ', 'Skycity', 'john@gmail.com', 29939492, 22, '0', '2024-01-20 13:28:59', '0000-00-00 00:00:00'),
+(37, 'bibek munikar', 'atrax group', 'bibek.munikar@gmail.com', 2041677367, 19, '0', '2024-01-21 11:37:13', '0000-00-00 00:00:00'),
+(38, 'bibek munikar', '', 'bibek.munikar@gmail.com', 2041677367, 21, '1', '2024-01-21 13:39:49', '0000-00-00 00:00:00'),
+(39, 'bibek munikar', 'atrax group', 'bibek.munikar@gmail.com', 2041677367, 21, '1', '2024-01-23 00:28:09', '0000-00-00 00:00:00'),
+(40, 'Ashma Maharjan', 'TVNZ', 'ashma@gmail.com', 202227367, 22, '0', '2024-01-23 00:30:44', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -74,7 +69,7 @@ ALTER TABLE `visitors`
 -- AUTO_INCREMENT for table `visitors`
 --
 ALTER TABLE `visitors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
