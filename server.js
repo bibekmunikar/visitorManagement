6530120b20d1f21ab1ff887e4d95572a482a8c55
soumyabkulkarni <150219767+soumyabkulkarni@users.noreply.github.com>
2024-01-24 20:14:54 +1300

    Changes done after collaborating
    Final chanes

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const path = require('path');
 const conn = require('./dbConfig');
 // const { Collapse } = require('bootstrap');
 // const { Console } = require('console');
+
+const nodemailer = require('nodemailer');
 const app = express();
 
 // Initialize flash
@@ -48,6 +50,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/src'));
 
 // use res.render to load up an ejs view file
+
+
+// Nodemailer setup
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'atraxgroup2024@gmail.com',
+      pass: 'atrax@2024'
+    }
+  });
 
 // Front end main page
 app.get('/', function(req, res) {
@@ -97,7 +109,7 @@ app.post('/checkin', (req, res) => {
     }
 
     // Assuming the 'visitors' table has columns: name, from, email, phone, employee_id, health_safety
-    const sql = `INSERT INTO visitors (name, \`from\`, email, phone, employee_id, health_safety) VALUES (?, ?, ?, ?, ?, ?)`;
+    const sql = `INSERT INTO front_visitors (name, \`from\`, email, phone, employee_id, health_safety) VALUES (?, ?, ?, ?, ?, ?)`;
 
     // Map the 'Yes' value to 1 and 'No' value to 0
     const healthSafetyValue = (q_2 === 'Yes') ? 1 : 0;
@@ -108,71 +120,25 @@ app.post('/checkin', (req, res) => {
             return res.status(500).send(`Error inserting visitor data: ${err.message}`);
         }
 
+        // Assuming the 'employees' table has columns: id, email
+        const employeeSql = 'SELECT id, email FROM employees WHERE id = ?';
+        conn.query(employeeSql, [employee_id], (employeeErr, employeeResult) => {
+            if (employeeErr) {
+                console.error('Error fetching employee data:', employeeErr);
+                return res.status(500).send(`Error fetching employee data: ${employeeErr.message}`);
+            }
+
+            const selectedEmployee = employeeResult[0];
+
+         // Send email notification
+        sendEmailNotification(name, selectedEmployee.email);
+
         console.log('Visitor data inserted successfully');
         res.redirect('/');
+
+        });
     });
 });
-
-
-
- 
-// app.post('/checkin', (req, res) => {
-//     const { name, from, email, phone, employee_id, health_safety } = req.body;
-
-//     // Validate required fields
-//     if (!name || !phone) {
-//         return res.status(400).send('Name and phone are required');
-//     }
-
-//     // Assuming the 'visitors' table has columns: name, from, email, phone, employee_id, health_safety
-//     const sql = `INSERT INTO visitors (name, \`from\`, email, phone, employee_id, health_safety) VALUES (?, ?, ?, ?, ?, ?)`;
-
-//     // Assuming you want to store "Yes" as 1 and "No" as 0 in the database
-//     const healthSafetyValue = (health_safety.toLowerCase() === 'yes') ? 1 : 0;
-
-//     // Handle the case when employee_id is empty
-//     const employeeIdValue = 28 || /* Provide a default employee_id here */ 'DEFAULT_VALUE';
-
-//     conn.query(sql, [name, from, email, phone, employeeIdValue, healthSafetyValue], (err, result) => {
-//         if (err) {
-//             console.error('Error inserting visitor data:', err);
-//             return res.status(500).send(`Error inserting visitor data: ${err.message}`);
-//         }
-
-//         console.log('Visitor data inserted successfully');
-//         res.redirect('/');
-//     });
-// });
-
-
-
-
-
-// app.post('/checkin', (req, res) => {
-//     // Handle visitor check-in
-//     var name = req.body.name;
-//     var from = req.body.from;
-//     var email = req.body.email;
-//     var phone = req.body.phone;
-//     var employee_id = req.body.employee_id;
-//     var health_safety = req.body.health_safety;
-
-//     var sql = `INSERT INTO visitors (name, from, email, phone, employee_id, health_safety) VALUES (?, ?, ?, ?, ?, ?)`;
-
-//     if (name && phone) {
-//         conn.query(sql, [name, from, email, phone, employee_id, health_safety], function (err, result) {
-//             if (err) {
-//                 console.error(err);
-//                 res.status(500).send('Internal Server Error');
-//             } else {
-//                 res.redirect('/');
-//             }
-//         });
-//     } else {
-//         res.status(400).send('Name and phone are required');
-//     }
-// });
-
 
 
 //SignOut page
@@ -243,6 +209,7 @@ app.post('/register', function(req, res) {
             if (err) throw err;
 			console.log('record inserted');
 			res.render('pages/login', { errormessage: "Registration successful" });
+			
 		})
 	}
 	else {
@@ -261,8 +228,6 @@ app.get('/logout',(req,res) => {
 app.get('/dashboard', function (req, res) {
 		if (req.session.loggedin) {
     res.render('pages/dashboard', {username:req.session.username});
-		} else {
-		res.redirect('/');
 	}
 });
 
@@ -352,7 +317,7 @@ app.post('/delete_visitors', function(req, res) {
 
 //SignIn/Out Manager page
 app.get('/visitorcontrol', function(req, res) {
-  res.render('pages/visitorcontrol');
+  res.render('pages/visitorcontrol', {username:req.session.username});
 });
 
 
@@ -497,72 +462,28 @@ app.post('/delete_employees', function(req, res) {
 	}
  });
 
-
-// test star
-// app.post('/add_employees', function(req, res) {
-//     if (req.session.loggedin) {
-//         var full_name = req.body.full_name;
-//         var alt_name = req.body.alt_name;
-//         var email = req.body.email;
-//         var mobile_number = req.body.mobile_number;
-//         // Uncomment the following line if 'department' is part of the form data
-//         // var department = req.body.department;
-//         var job_title = req.body.job_title;
-//         // Uncomment the following line if 'emp_role_type' is part of the form data
-//         // var emp_role_type = req.body.emp_role_type;
-
-//         if (email) {
-//             // Uncomment the following lines if 'department' and 'emp_role_type' are part of the database schema
-//             // var sql = `INSERT INTO employees (full_name, alt_name, email, mobile_number, department, job_title, emp_role_type, photo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-//             // conn.query(sql, [full_name, alt_name, email, mobile_number, department, job_title, emp_role_type, photo], function(err, result) {
-
-//             // If 'department' and 'emp_role_type' are not part of the database schema, use the following line
-//             var sql = `INSERT INTO employees (full_name, alt_name, email, mobile_number, job_title) VALUES (?, ?, ?, ?, ?)`;
-            
-//             conn.query(sql, [full_name, alt_name, email, mobile_number, job_title], function(err, result) {
-//                 if (err) {
-//                     console.error(err);
-//                     res.status(500).send('Internal Server Error');
-//                 } else {
-//                     console.log('Record inserted');
-//                     res.redirect('/employees');
-//                 }
-//             });
-//         } else {
-//             console.log("Error: Employee name is missing");
-//             res.status(400).send('Bad Request');
-//         }
-//     } else {
-//         res.redirect('/');
-//     }
-// });
-// test end 
-
-
-
- // Define a route to fetch data for the dropdown
-// app.get('/employees', (req, res) => {
-//   const query = 'SELECT id, department_name FROM departments';
-//   db.query(query, (err, results) => {
-//     if (err) {
-//       console.error('Error executing query:', err);
-//       return res.status(500).send('Internal Server Error');
-//     }
-//     // Render a template with the retrieved data
-//     res.render('employees', { departmentData: results });
-//   });
-// });
-
-//Dashboard page
-app.get('/dashboard', function(req, res) {
-  res.render('pages/dashboard');
-});
 // login page
 app.get('/404', function(req, res) {
     res.render('pages/404');
   });
-  
 
+
+  function sendEmailNotification(visitorName, employeeEmail) {
+    const mailOptions = {
+      from: 'atraxgroup2024@gmail.com',
+      to: employeeEmail,
+      subject: 'Visitor Sign-In Notification',
+      text: `Hello Visitor ${visitorName} has signed in.`
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
+  }
 
 // about page
 // app.get('/about', function(req, res) {
